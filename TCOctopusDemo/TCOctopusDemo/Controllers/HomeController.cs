95274060da8a8b345d729efd003a2213974250a8
Juan Francisco Diaz <juan.diaz@prodigious.com>
2015-07-02 07:44:18 -0600

    code added

---
+++ TCOctopusDemo/TCOctopusDemo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Configuration;
+
+namespace TCOctopusDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string name = WebConfigurationManager.AppSettings["ApplicationName"];
+            ViewBag.Message = String.Format("{0} this template to jump-start your ASP.NET MVC application.", name);
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TCOctopusDemo/TCOctopusDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TCOctopusDemo.MvcApplication" Language="C#" %>
